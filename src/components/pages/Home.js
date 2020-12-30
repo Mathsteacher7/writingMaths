67d13704b6fd57bcda9de67b9dfcc9afe7a65f60
Daniel Cohen <dcohen@wundermancommerce.com>
2020-12-30 23:03:21 +0000

    writing formula display block and style with the right classes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,13 +3,12 @@ import React from "react";
 import Mathjax from "react-mathjax2";
 
 const ascii = "U = 1/(R_si)";
-const tex = 'f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi'
-const array = '\\begin{array}{cc} a & b \\\\ c & d \\end{array}';
-const math = '\\frac{1}{\\sqrt{x^2 + 1}}';
-const det = 'x_{1,2}=\\frac{b\\pm\\sqrt{b^2-4ac}}{2}'
-const dividing = '\\frac{1}{2}'
-
-
+const tex =
+  "f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi";
+const array = "\\begin{array}{cc} a & b \\\\ c & d \\end{array}";
+const math = "\\frac{1}{\\sqrt{x^2 + 1}}";
+const det = "x_{1,2}=\\frac{b\\pm\\sqrt{b^2-4ac}}{2}";
+const dividing = "\\frac{1}{2}";
 
 const Home = () => {
   return (
@@ -23,53 +22,62 @@ const Home = () => {
           <Mathjax.Node inline>{ascii}</Mathjax.Node>
         </div>
       </Mathjax.Context>
-      <br/>
+      <br />
       <Mathjax.Context input="tex">
         <div>
           This is an inline formula written in AsciiMath:{" "}
           <Mathjax.Node inline>{tex}</Mathjax.Node>
         </div>
       </Mathjax.Context>
-      <br/>
+      <br />
       <Mathjax.Context input="tex">
         <div>
           This is an inline formula written in AsciiMath:{" "}
           <Mathjax.Node inline>{array}</Mathjax.Node>
         </div>
       </Mathjax.Context>
-      <br/>
+      <br />
       <Mathjax.Context input="tex">
         <div>
           This is an inline formula written in AsciiMath:{" "}
           <Mathjax.Node inline>{math}</Mathjax.Node>
         </div>
       </Mathjax.Context>
-      <br/>
+      <br />
       <Mathjax.Context input="tex">
         <div>
           This is an inline formula written in AsciiMath:{" "}
           <Mathjax.Node inline>{math}</Mathjax.Node>
         </div>
       </Mathjax.Context>
-      <br/>
+      <br />
       <Mathjax.Context input="tex">
         <div>
           This is an inline formula written in AsciiMath:{" "}
           <Mathjax.Node inline>{math}</Mathjax.Node>
         </div>
       </Mathjax.Context>
-      <br/>
+      <br />
       <Mathjax.Context input="tex">
         <div>
           This is an inline formula written in AsciiMath:{" "}
           <Mathjax.Node inline>{det}</Mathjax.Node>
         </div>
       </Mathjax.Context>
-      <br/>
+      <br />
       <Mathjax.Context input="tex">
         <div>
           This is an inline formula written in AsciiMath:{" "}
           <Mathjax.Node inline>{dividing}</Mathjax.Node>
+        </div>
+      </Mathjax.Context>
+      <br />
+      <Mathjax.Context input="tex">
+        <div>
+          find x if we know that{" "}
+          <Mathjax.Node>
+            {dividing}
+          </Mathjax.Node>
         </div>
       </Mathjax.Context>
     </section>
